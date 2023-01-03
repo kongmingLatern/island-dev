96a34db888deb88fc285f1dd416496ded3cf9c12
凤之兮原 <1577310596@qq.com>
2023-01-03 15:48:18 +0800

    feat: use plugin

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,9 @@
 import { createServer } from 'vite'
+import { pluginIndexHtml } from './plugin-island/indexHTML'
 
 export function createDevServer(root = process.cwd()) {
   return createServer({
     root,
+    plugins: [pluginIndexHtml()],
   })
 }
