0ad7f835beb933ce27a3fc6fe68874681a6e9f2d
凤之兮原 <1577310596@qq.com>
2023-01-04 21:56:46 +0800

    feat: add plugin about indexHTML

--- src/node/plugin-island/indexHTML.ts
+++ src/node/plugin-island/indexHTML.ts
@@ -1,15 +1,37 @@
 import type { Plugin } from 'vite'
 import { readFile } from 'fs/promises'
+import { CLIENT_ENTRY_PATH } from '../constants'
 export function pluginIndexHtml(): Plugin {
   return {
     name: 'island:index-html',
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: 'body',
+          },
+        ],
+      }
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
           // 1. 读取 template.html 内容
-          const content = await readFile(
-            'template.html',
+          let content = await readFile(
+            'index.html',
             'utf-8'
+          )
+          // 热更新
+          content = await server.transformIndexHtml(
+            req.url,
+            content,
+            req.originalUrl
           )
           // 2. 响应 html 浏览器
           res.setHeader('Content-Type', 'text/html')
