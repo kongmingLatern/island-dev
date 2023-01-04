37953cfd6e2d7aff7e0c91ee68d0031008b1b724
凤之兮原 <1577310596@qq.com>
2023-01-04 21:47:46 +0800

    chore: add build

---
+++ src/node/build.ts
@@ -0,0 +1,44 @@
+import { build as viteBuild } from 'vite'
+import {
+  CLIENT_ENTRY_PATH,
+  SERVER_ENTRY_PATH,
+} from './constants'
+export async function bundle(root: string) {
+  try {
+    const clientBuild = async () => {
+      return viteBuild({
+        mode: 'production',
+        root,
+        build: {
+          outDir: 'build',
+          rollupOptions: {
+            input: CLIENT_ENTRY_PATH,
+            output: {
+              format: 'esm',
+            },
+          },
+        },
+      })
+    }
+    const serverBuild = async () => {
+      return viteBuild({
+        mode: 'production',
+        root,
+        build: {
+          outDir: '.temp',
+          rollupOptions: {
+            input: SERVER_ENTRY_PATH,
+            output: {
+              format: 'cjs',
+            },
+          },
+        },
+      })
+    }
+    await clientBuild()
+    await serverBuild()
+  } catch (e) {}
+}
+export async function build(root: string = process.cwd()) {
+  await bundle(root)
+}
