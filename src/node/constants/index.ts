28cb1d623bc179c6fddccd1f101fcf63080f4e22
凤之兮原 <1577310596@qq.com>
2023-01-04 21:46:29 +0800

    chore: add two constant

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -1,13 +1,22 @@
 import * as path from 'path'
 
-export const PACKAGE_ROOT = path.join(
-  '__dirname',
-  '..',
-  '..',
-  '..'
+export const PACKAGE_ROOT = path.join('__dirname', '..')
+
+export const CLIENT_ENTRY_PATH = path.join(
+  PACKAGE_ROOT,
+  'src',
+  'runtime',
+  'client-entry.tsx'
+)
+
+export const SERVER_ENTRY_PATH = path.join(
+  PACKAGE_ROOT,
+  'src',
+  'runtime',
+  'ssr-entry.tsx'
 )
 
 export const DEFAULT_TEMPLATE_PATH = path.join(
   PACKAGE_ROOT,
-  'template.html'
+  'index.html'
 )
