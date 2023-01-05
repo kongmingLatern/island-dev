f45f2b2baed28166464fd5ab5d012f4fd7d58c1d
凤之兮原 <1577310596@qq.com>
2023-01-05 14:24:12 +0800

    chore: add plugin-react plugin

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer } from 'vite'
 import { pluginIndexHtml } from './plugin-island/indexHTML'
+import pluginReact from '@vitejs/plugin-react'
 
-export function createDevServer(root = process.cwd()) {
+export function createDevServer(root: string) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml()],
+    plugins: [pluginIndexHtml(), pluginReact()],
   })
 }
