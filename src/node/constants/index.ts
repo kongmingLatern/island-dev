72eb55951ab3074ea59ca2dc11e2ed433a3ccb21
凤之兮原 <1577310596@qq.com>
2023-01-05 14:24:25 +0800

    change: change constant

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -1,22 +1,29 @@
-import * as path from 'path'
+import { join } from 'path'
 
-export const PACKAGE_ROOT = path.join('__dirname', '..')
+export const PACKAGE_ROOT = join(
+  __dirname,
+  '..',
+  '..',
+  '..'
+)
 
-export const CLIENT_ENTRY_PATH = path.join(
+export const RUNTIME_PATH = join(
   PACKAGE_ROOT,
   'src',
-  'runtime',
+  'runtime'
+)
+
+export const CLIENT_ENTRY_PATH = join(
+  RUNTIME_PATH,
   'client-entry.tsx'
 )
 
-export const SERVER_ENTRY_PATH = path.join(
-  PACKAGE_ROOT,
-  'src',
-  'runtime',
+export const SERVER_ENTRY_PATH = join(
+  RUNTIME_PATH,
   'ssr-entry.tsx'
 )
 
-export const DEFAULT_TEMPLATE_PATH = path.join(
+export const DEFAULT_HTML_PATH = join(
   PACKAGE_ROOT,
-  'index.html'
+  'template.html'
 )
